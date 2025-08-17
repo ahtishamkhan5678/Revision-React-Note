57629e8b293e3054ec25bd245b12db7e8e40d746
mohammadAhtisham <mohammadahtisham@mohammadAhtishams-MacBook-Air.local>
2025-08-17 20:16:34 +0530

    inital-setup

---
+++ Ep-1/App.js
@@ -0,0 +1,49 @@
+
+
+
+// const heading = React.createElement("h1", {
+//     id: "heading",
+//     test: "testing user"
+// }, "I m from React");
+
+// console.log("heading", heading)
+
+
+/*
+  <div id="parent">  
+        <div id = "child"> 
+            <h1>Hell Adil</h1>
+            <h2> Hell Ahtisham</h2>
+        </div>
+  </div>
+    
+
+*/
+
+const parent = React.createElement("div",
+    {
+        id: "parent"
+    },
+    [
+        React.createElement("div", {
+            id: "child"
+        },
+            [
+                React.createElement("h1", {}, "Hi im h1"),
+                React.createElement("h2", {}, "Hi im h2")
+            ]
+        ),
+        React.createElement("div", {
+            id: "child"
+        },
+            [
+                React.createElement("h1", {}, "Hi im h1"),
+                React.createElement("h2", {}, "Hi im h2")
+            ]
+        )
+    ]
+
+)
+
+const root = ReactDOM.createRoot(document.getElementById("adil"));
+root.render(parent);
